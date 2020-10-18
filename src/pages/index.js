477de2f93c3c6d25f6f92a6e066bfc6c127b6591
Sulhan Jauhary <ms.jauhary@gmail.com>
2020-10-18 23:45:29 +0700

    update readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi guys</h1>
-    <p>Welcome to my static web :)</p>
+    <h1>Hi guys!!!</h1>
+    <p>Welcome to my static web :(</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
