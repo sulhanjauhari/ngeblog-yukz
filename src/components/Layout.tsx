d3b91408199afc06924f71bec31f9686b14573b2
Sulhan Jauhary Personal <ms.jauhary@gmail.com>
2021-01-09 13:32:26 +0700

    update footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,7 +15,7 @@ const Layout: React.FC = ({ children }) => {
         }}>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built with love using
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
