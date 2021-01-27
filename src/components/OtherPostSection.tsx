456d2adeafa832d8a3ba9399cc6da768b62c290e
Sulhan Jauhary Personal <ms.jauhary@gmail.com>
2021-01-27 17:56:02 +0700

    feat: add arrow icon

--- src/components/OtherPostSection.tsx
+++ src/components/OtherPostSection.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "gatsby";
 import styled from "@emotion/styled";
+import ArrowLeft from "./icons/ArrowLeft";
+import ArrowRight from "./icons/ArrowRight";
+import { css } from "@emotion/core";
 
 const OtherPostWrapper = styled.ul`
   display: flex;
@@ -8,6 +11,17 @@ const OtherPostWrapper = styled.ul`
   justify-content: space-between;
   list-style: none;
   padding: unset;
+
+  li {
+    svg {
+      width: 24px;
+    }
+
+    a {
+      display: flex;
+      align-items: center;
+    }
+  }
 `;
 
 const OtherPostSection: React.FC<OtherPostSectionProp> = props => {
@@ -15,8 +29,22 @@ const OtherPostSection: React.FC<OtherPostSectionProp> = props => {
   return (
     <section css={{ marginBottom: "6rem" }}>
       <OtherPostWrapper>
-        <li>{prev && <Link to={prev.slug}>{prev.title}</Link>}</li>
-        <li>{next && <Link to={next.slug}>{next.title}</Link>}</li>
+        <li>
+          {prev && (
+            <Link to={prev.slug}>
+              <ArrowLeft />
+              &nbsp; {prev.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={next.slug}>
+              {next.title} &nbsp;
+              <ArrowRight />
+            </Link>
+          )}
+        </li>
       </OtherPostWrapper>
     </section>
   );
